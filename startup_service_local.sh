8fd73c6ace5e806e4c7d9145d0286e7dbbe7fee0
Kovalevaolya <kovaleva.olga.2004@gmail.com>
2024-12-25 20:43:35 +0300

    поправлена ошибка

--- startup_service_local.sh
+++ startup_service_local.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-source source .venv/bin/activate
-exec gunicorn --bind 0.0.0.0:10000 --workers 4 quiz_gen_django.wsgi:application
+source .venv/bin/activate
+exec gunicorn --bind 0.0.0.0:10000 --timeout 450 --workers 4 quiz_gen_django.wsgi:application
